a766fc496329c9d4e51d3325d0bc121f3b7cd1eb
Mohammed-Ali-Elghannam <mohammedelghannam08@gmail.com>
2025-04-29 01:48:24 +0300

    Update urls.py

--- hotel/urls.py
+++ hotel/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('register/', views.register_view, name='register'),
     path('verify-email/<int:user_id>/<str:token>/', views.verify_email, name='verify_email'),
     path('logout/', views.logout_view, name='logout'),
+    path('search/', views.hotel_search, name='hotel_search'),
+    path('hotel/<int:pk>/', views.hotel_detail, name='hotel_detail'), 
 ]
